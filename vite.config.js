465391cff058fa0bfc4d45dfdfb3445c8d3e5f29
unknown <sevakmanvelyan@gmail.com>
2024-07-07 20:00:30 +0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<gh-pages>/', // Replace with your GitHub repository name
+  base: '/weather/', // Replace with your GitHub repository name
 });
